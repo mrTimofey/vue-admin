2a17859ccb3ce7752b01ce2be98f5ca1c961d98d
timoxa <timoxa17@gmail.com>
2017-11-30 09:24:21 +0300

    minor fix

--- src/components/entity/header.vue
+++ src/components/entity/header.vue
@@ -1,23 +1,14 @@
-<script>
-	export default {
-		name: 'EntityHeader',
-		props: {
-			title: String,
-			subtitle: String
-		}
-	};
-</script>
-<template lang="pug">
+<template functional lang="pug">
 	.content-header
-		h1.content-title!='{{ title }} '
-			small(v-if="subtitle") {{ subtitle }}
+		h1.content-title!='{{ props.title }} '
+			small(v-if="props.subtitle") {{ props.subtitle }}
 		!=' '
 		slot
 		ol.breadcrumb
 			li
 				i.fa.fa-dashboard
 				!=' '
-				router-link(to="/") {{ $t('home') }}
+				router-link(to="/") {{ parent.$t('home') }}
 			slot(name="breadcrumbs")
-				li.active: span {{ title }}
+				li.active: span {{ props.title }}
 </template>
