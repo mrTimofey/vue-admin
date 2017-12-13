d5ea4775659dd1d59ec40717f7be37e0d443a915
timoxa <timoxa17@gmail.com>
2017-12-13 10:00:34 +0300

    fix update event on number field

--- src/components/fields/number.vue
+++ src/components/fields/number.vue
@@ -11,7 +11,7 @@
 			},
 			min: {
 				type: Number,
-				default: 0
+				default: -Infinity
 			},
 			max: {
 				type: Number,
@@ -20,10 +20,15 @@
 		},
 		methods: {
 			emitValue(e) {
-				const old = e.target.value.toString(),
-					v = old.replace(/(?!^-)[^0-9]/g, '');
-				if (v !== old) e.target.value = v;
-				if (v !== '-') this.$emit('input', v === '' ? null : parseInt(v));
+				const real = e.target.value.toString(),
+					v = real.replace(/(?!^-)[^0-9]/g, '');
+				if (v !== real) {
+					e.target.value = v;
+				}
+				if (v !== '-') {
+					const newValue = v === '' ? null : parseInt(v);
+					if (this.value !== newValue) this.$emit('input', newValue);
+				}
 			},
 			onBlur() {
 				if (this.value === null) return;
