b003a13bf338c9e44aecb4f49a788b7b634bdf21
timoxa <timoxa17@gmail.com>
2018-02-17 13:30:12 +0300

    fix array field to work with objects

--- src/components/displays/array.vue
+++ src/components/displays/array.vue
@@ -2,13 +2,13 @@
 	export default {
 		functional: true,
 		props: {
-			value: Array,
+			value: null,
 			itemProps: Object
 		}
 	};
 </script>
 <template functional lang="pug">
-	span(v-if="props.value && props.value.length")
+	span(v-if="props.value && (props.value.length || Object.keys(props.value).length)")
 		display(v-for="(item, i) in props.value"
 			v-bind="props.itemProps || {}"
 			':key'="i"
